362385f1e1fb1f35eb3ef8308b53e3b7bc0fe38a
Onur <onurburak@sabanciuniv.edu>
2017-03-10 02:23:10 -0800

    header change to test

--- server.js
+++ server.js
@@ -26,12 +26,12 @@ app.get('/viewImage', function(request, response) {
     console.log("Request received from : /viewImage");
     //response.setHeader("content-Type", "multipart/x-mixed-replace; boundary=--frame\r\n\r\n");
     response.writeHead(200, {
-            'Connection': 'Close',
-            'Expires': '-1',
-            'Cache-Control': 'no-store, no-cache, must-revalidate, max-age=0, post-check=0, pre-check=0, false',
-            'Pragma': 'no-cache',
-            'Content-Type': 'multipart/x-mixed-replace; boundary=frameFRAMEframe'
-        });
+        'Connection': 'Close',
+        'Expires': '-1',
+        'Cache-Control': 'no-store, no-cache, must-revalidate, max-age=0, post-check=0, pre-check=0, false',
+        'Pragma': 'no-cache',
+        'Content-Type': 'multipart/x-mixed-replace; boundary=frameFRAMEframe'
+    });
     response.useChunkedEncodingByDefault = false;
     getRequestTriggered = true;
     requestObject = response;
@@ -41,14 +41,14 @@ wsServer = new WebSocketServer({
     httpServer: server,
     autoAcceptConnections: false
 });
-
+var clients = [];
 wsServer.on('request', function(request) {
 
 
     var connection = request.accept('', request.origin);
     console.log((new Date()) + ' Connection accepted.');
-
-    connection.sendUTF("ONURBURAK");
+    clients.push(connection);
+    connection.sendUTF(request.origin);
     connection.on('message', function(message) {
         if (message.type === 'utf8') {
             console.log('1-Received Message: ' + message.utf8Data);
@@ -57,7 +57,10 @@ wsServer.on('request', function(request) {
                 requestObject.write("\r\n");
                 console.log(requestObject.write(message.utf8Data, function() {
                     console.log('Write succesful-header');
-                    connection.sendUTF("MERABA");
+                    for (var i = 0; i < clients.length; i++) {
+                        clients[i].sendUTF("Thanks!");
+                    }
+                    //connection.sendUTF("MERABA");
 
                 }))
             }
