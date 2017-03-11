831e97aaf61f67ca04ae0d06184bf4d73fd72b23
Onur <onurburak@sabanciuniv.edu>
2017-03-10 16:46:56 -0800

    frame

--- server.js
+++ server.js
@@ -70,11 +70,9 @@ wsServer.on('request', function(request) {
                 requestObject.write(message.binaryData, function() {
                     //console.log('Write succesful-image');
                     console.log(message.binaryData);
+                    
                     for (var i = 0; i < clients.length; i++) {
-                        var byteArray = new Uint8Array(message.binaryData);
-                        for (var x = 0; x < byteArray.byteLength; x++) {
-                            var buffer = new Buffer(byteArray[x]);
-                            clients[i].sendBytes(buffer);
+                            clients[i].sendBytes(message.binaryData);
                         }
 
                     }
