e35570b5def3844974fb0872f8811cd48cd260fd
Onur <onurburak@sabanciuniv.edu>
2017-03-11 16:10:42 -0800

    frame

---
+++ html/scriptCarEngine.js
@@ -0,0 +1,143 @@
+var socket
+
+$(document).ready(function() {
+  var lastMove = 0;
+  //socket connection and event
+
+
+  $("a.forward").click(function() {
+    move(800, 800);
+  });
+  $("a.back").click(function() {
+    move(-800, -800);
+  });
+  $("a.left").click(function() {
+    move(-800, 800);
+  });
+  $("a.right").click(function() {
+    move(800, -800);
+  });
+
+
+  $("a.start").click(function() {
+    socket.send("start");
+  });
+  $("a.stop").click(function() {
+    move(0,0);
+    socket.send("stop");
+
+  });
+
+  //Check for if it's a mobile device or not
+  if (window.DeviceMotionEvent) {
+    window.addEventListener('devicemotion', deviceMotionHandler, false);
+  } else {
+    document.getElementById("dmEvent").innerHTML = "Accelerometer not supported."
+  }
+
+  //For Keyboard WASD
+  document.onkeydown = function detectKey(event) {
+    var e = event.keyCode;
+    if (e == 87) { //W
+      move(800, 800);
+    }
+    if (e == 83) { //S
+      move(-800, -800);
+    }
+    if (e == 65) { //A
+      move(-800, 800);
+    }
+    if (e == 68) { //D
+      move(800, -800);
+    }
+  }
+
+  function move(left, right) {
+    var now = Date.now();
+    if (lastMove + 200 < now) {
+      lastMove = now;
+      var obj = {
+        left,
+        right
+      };
+      socket.send(JSON.stringify(obj));
+    }
+  }
+
+
+  function deviceMotionHandler(eventData) {
+    acceleration = eventData.accelerationIncludingGravity;
+    var left = 0;
+    var right = 0;
+    if (Math.abs(acceleration.y) > 1) { // back-/forward
+      var speed = acceleration.y * 150;
+      left = Math.min(1023, speed + acceleration.x * 100);
+      right = Math.min(1023, speed - acceleration.x * 100);
+    } else if (Math.abs(acceleration.x) > 1) {
+      var speed = Math.min(1023, Math.abs(acceleration.x) * 123);
+      if (acceleration.x > 0) {
+        left = speed;
+        right = -speed;
+      } else {
+        left = -speed;
+        right = speed;
+      }
+    }
+    if (Math.abs(left) > 100 || Math.abs(right) > 100) {
+      if(left<-1023){
+        left=-1023;
+      }
+      if(right<-1023){
+        right=-1023;
+      }
+      move(left, right);
+    }
+    var direction = "stop";
+    direction = "[" + Math.round(acceleration.x) + "," + Math.round(acceleration.y) + "," + Math.round(acceleration.z) + "]<BR/>" + Math.round(left) + ", " + Math.round(right);
+    document.getElementById("vector").innerHTML = direction;
+  }
+});
+ function connect() {
+
+   window.WebSocket = window.WebSocket || window.MozWebSocket;
+   var url=$("#cameraIP").val();
+   socket = new WebSocket(url);
+   socket.addEventListener("open", function(event) {
+     console.log("Connected");
+   });
+
+   // Display messages received from the server
+   socket.addEventListener("message", function(event) {
+     console.log(typeof event.data);
+     console.log(event.data);
+
+     var urlObj = window.URL || window.webkitURL;
+     //var imageUrl = urlCreator.createObjectURL(event.data);
+     if (typeof event.data != "string") {
+       var blob = event.data;
+       var url = window.URL.createObjectURL(blob);
+       console.log(blob);
+       console.log('Created a png blob of size: ' + blob.size);
+       console.log('Inserting an img...');
+       document.querySelector("#image").src = url;
+       document.querySelector("#image").onload = function(){
+        window.URL.revokeObjectURL(this.src);
+       }
+
+       console.log('Blob URL is: ' + url);
+
+      
+     }
+
+   });
+
+   // Display any errors that occur
+   socket.addEventListener("error", function(event) {
+     console.log("Error: " + event);
+   });
+
+   socket.addEventListener("close", function(event) {
+     open.disabled = false;
+     console.log("Not Connected");
+   });
+ };
--- server.js
+++ server.js
@@ -25,17 +25,31 @@ app.use('/', express.static("html"));
 app.get('/viewImage', function(request, response) {
     console.log("Request received from : /viewImage");
     response.setHeader("content-Type", "multipart/x-mixed-replace; boundary=frameFRAMEframe\r\n\r\n");
-    // response.writeHead(200, {
-    //     'Connection': 'Close',
-    //     'Expires': '-1',
-    //     'Cache-Control': 'no-store, no-cache, must-revalidate, max-age=0, post-check=0, pre-check=0, false',
-    //     'Pragma': 'no-cache',
-    //     'Content-Type': 'multipart/x-mixed-replace; boundary=frameFRAMEframe'
-    // });
     response.useChunkedEncodingByDefault = false;
     getRequestTriggered = true;
     requestObject = response;
 });
+app.get('/getValues', function(req, res) {
+    if (isWorking)
+        res.send({
+            left: left,
+            right: right
+        });
+    else
+        res.send({
+            left: 0,
+            right: 0
+        });
+});
+app.get('/reset', function(req, res) {
+            left = 0;
+            right = 0;
+
+            res.send({
+                code: 200,
+                message: 'SUCCESS'
+            });
+        });
 
 wsServer = new WebSocketServer({
     httpServer: server,
@@ -50,6 +64,7 @@ wsServer.on('request', function(request) {
     if (request.origin == "http://rcteer.swastibhat.com")
         clients.push(connection);
     connection.on('message', function(message) {
+        console.log(message);
         if (message.type === 'utf8') {
             // console.log('1-Received Message: ' + message.utf8Data);
             //requestObject.write("--frame\r\n");
@@ -60,7 +75,6 @@ wsServer.on('request', function(request) {
             connection.sendUTF("text received");
         } else if (message.type === 'binary') {
             //console.log('2-Received Binary Message of ' + message.binaryData.length + ' bytes');
-            console.log(message.binaryData);
 
             for (var i = 0; i < clients.length; i++) {
                 clients[i].sendBytes(message.binaryData);
